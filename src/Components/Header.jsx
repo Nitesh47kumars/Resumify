cadb3f1ed59895707627f26b80f144407549c8a7
Nitesh47kumars <niteshshah028@gmail.com>
2025-10-25 21:49:08 +0530

    Reduce Redundancy & Length

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
 
 const Header = () => {
+
+    const links = [
+        {name: "About", link: "about"},
+        {name: "Features", link: "features"},
+        {name: "Templates", link: "templates"},
+        {name: "Q & A", link: "qa"},
+    ]
+
   return (
     <header className="flex justify-between items-center px-6 py-5 bg-gray-50 shadow-xl">
 
@@ -10,10 +18,11 @@ const Header = () => {
         </div>
 
         <div className="hidden md:flex space-x-10 pe-10">
-            <button className="text-sm font-medium text-gray-700 hover:text-indigo-600">Personal Info</button>
-            <button className="text-sm font-medium text-gray-700 hover:text-indigo-600">Education</button>
-            <button className="text-sm font-medium text-gray-700 hover:text-indigo-600">Experience</button>
-            <button className="text-sm font-medium text-gray-700 hover:text-indigo-600">Skills</button>
+            {links.map((link)=>{
+                return <button className="text-sm font-medium text-gray-700 hover:text-indigo-600">
+                    {link.name}
+                </button>
+            })}
         </div>
     </header>
 
