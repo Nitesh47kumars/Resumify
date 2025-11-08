5dd9ef93c00fdba840cda462c8960b3446088029
Nitesh47kumars <niteshshah028@gmail.com>
2025-11-08 18:27:43 +0530

    Change Header into Navbar & Build it for Mobile

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Header from './Components/Header'
+import Header from './Components/Navbar'
 import HeroSection from './Components/HeroSection/HeroSection'
 import AboutSection from './Components/About/AboutSection'
 import Features from './Components/Features/Features'
