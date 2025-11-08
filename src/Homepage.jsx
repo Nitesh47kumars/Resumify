2a7431a75edd5f5719aa40fcaab1e54953ae890f
Nitesh47kumars <niteshshah028@gmail.com>
2025-11-08 19:01:20 +0530

    Build Template Component

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -3,6 +3,7 @@ import Header from './Components/Navbar'
 import HeroSection from './Components/HeroSection/HeroSection'
 import AboutSection from './Components/About/AboutSection'
 import Features from './Components/Features/Features'
+import TemplatesSection from './Components/Template/Template'
 
 const Homepage = () => {
   return (
@@ -11,6 +12,7 @@ const Homepage = () => {
       <HeroSection/>
       <AboutSection/>
       <Features/>
+      <TemplatesSection/>
     </div>
   )
 }
